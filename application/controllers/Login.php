4b8b5835f10caaab36b184f8d20638cdcd8c9fea
nauflhaq <naufalhaq24@gmail.com>
2018-05-17 19:54:02 +0700

    update1

---
+++ application/controllers/Login.php
@@ -0,0 +1,43 @@
+<?php
+ 
+class Login extends CI_Controller{
+ 
+    function __construct(){
+        parent::__construct();     
+        $this->load->model('Model_login');
+ 
+    }
+ 
+    function index(){
+        $this->load->view('View_login');
+    }
+ 
+    function aksi_login(){
+        $username = $this->input->post('username');
+        $password = $this->input->post('password');
+        $where = array(
+            'username' => $username,
+            'password' => md5($password)
+            );
+        $cek = $this->Model_login->cek_login("admin",$where)->num_rows();
+        if($cek > 0){
+ 
+            $data_session = array(
+                'nama' => $username,
+                'status' => "login"
+                );
+ 
+            $this->session->set_userdata($data_session);
+ 
+            redirect(base_url("index.php/Home"));
+ 
+        }else{
+            echo "Username dan password salah !";
+        }
+    }
+ 
+    function logout(){
+        $this->session->sess_destroy();
+        redirect(base_url('index.php/Login'));
+    }
+}
---
+++ application/views/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php $this->load->view("component/header"); ?>
+<?php $this->load->view("component/sidebar"); ?>
+<?php $this->load->view("component/footer"); ?>
+</body>
+</html>
+ 
+
+ 
